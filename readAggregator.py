a9c833dd3f5e455530f2a44596669864cafa5bed
TristinSmith06 <tristin.smith@students.fhu.edu>
2026-06-25 09:25:37 -0500

    Add base composition plot

--- readAggregator.py
+++ readAggregator.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 import bamParsing
 
 #Make a linechart of U counts at each position
@@ -38,14 +39,31 @@ def make_aggregate_summary_stats(reads):
     min_seq_len.metric("Min Read Length", sequence_lengths.min(), border=True)
     max_seq_len.metric("Max Read Length", sequence_lengths.max(), border=True)
 
+
+def get_canonical_base_counts(reads) -> Counter:
+    base_counts = Counter()
+    for r in reads:
+        base_counts.update(r.sequence)
+    return base_counts
+    
+
 def aggregate_file(bam, U_thresh):
-    filtered_reads, num_reads_in_file = bamParsing.get_everything(bam)
+    with st.spinner("Extracting reads from file, please wait..."):
+        filtered_reads, num_reads_in_file = bamParsing.get_everything(bam)
+    
     st.info(f"Aggregated {len(filtered_reads)} reads of length 80+ from {num_reads_in_file} reads")
     
     make_aggregate_summary_stats(filtered_reads)
     
-
-    counts = get_sum_of_u_positions(filtered_reads, U_thresh, 50)
+    with st.spinner("Generating canonical base proportions..."):
+        cannonical_base_counts = get_canonical_base_counts(filtered_reads)
+        prop = pd.DataFrame(cannonical_base_counts.items(), columns=['Base', 'Count'])
+        
+        #Make bases always appear alphabetically
+        prop.sort_values(by='Base', inplace=True)
+        st.bar_chart(data = prop, x="Base", y="Count", horizontal=True, color="Base")
     
+    counts = get_sum_of_u_positions(filtered_reads, U_thresh, 50)
     make_U_count_line_graph(counts)
+    
     
